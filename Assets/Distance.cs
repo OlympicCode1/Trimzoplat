b2ed5fc8472bf307e26e8838daf43043163c6f91
OlympicCode1 <dkkaptan1@gmail.com>
2024-11-30 21:24:40 +0300

    s

--- Assets/Distance.cs
+++ Assets/Distance.cs
@@ -10,13 +10,12 @@ public class Distance : MonoBehaviour
     void Start()
     {
         player = GameObject.Find("Player").GetComponent<Transform>();
-    
     }
 
     // Update is called once per frame
     void Update()
     {
-        float Distance = player.position.x;
-        asd.text = "a";
+        float Distance = player.position.x + 20.0397f;
+        asd.text = Distance.ToString();
     }
 }
